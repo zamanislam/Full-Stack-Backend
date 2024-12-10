1fe5d46520907b83aa16c046eaea95cdf015dffe
zamanislam <zamni0123@gmail.com>
2024-12-10 09:59:19 +0530

    code pushed

--- index.js
+++ index.js
@@ -7,19 +7,12 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
-// Allowed origins
-const allowedOrigins = ['http://localhost:5173', 'https://zamway.netlify.app'];
-
-// CORS middleware
+// Enable CORS for all origins
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS not allowed from this origin'));
-    }
-  },
-  credentials: true, // Include cookies and auth headers
+  origin: "*", // Allow all origins
+  credentials: false, // Set to true if you need cookies/auth headers
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 }));
 
 // Body parser middleware
@@ -32,15 +25,6 @@ app.use("/products", productRouter);
 // Health check route
 app.get("/health", (req, res) => {
   res.send("OK!");
-});
-
-// Error handler for CORS issues (optional, for debugging)
-app.use((err, req, res, next) => {
-  if (err.message === 'CORS not allowed from this origin') {
-    res.status(403).json({ error: 'CORS not allowed' });
-  } else {
-    next(err);
-  }
 });
 
 // Start the server
