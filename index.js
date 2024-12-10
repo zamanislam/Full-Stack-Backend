3d2171b541c111b10236f85663be2e6a33bdd1cc
zamanislam <zamni0123@gmail.com>
2024-12-10 10:36:08 +0530

    code pushed

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const express = require("express");
 const connect = require("./config/db");
 const userRouter = require("./routes/user.route");
 const productRouter = require("./routes/blog.route");
+const app = express();
 const cors = require("cors");
 
-const app = express();
 require("dotenv").config();
 
-// Enable CORS for all origins
 app.use(cors({
-  origin: "*", // Allow all origins
-  credentials: false, // Set to true if you need cookies/auth headers
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-}));
+  origin:process.env.FRONTEND_URL
+}))
+
 
 // Body parser middleware
 app.use(express.json());
+
+
 
 // Routes
 app.use("/users", userRouter);
@@ -25,6 +24,12 @@ app.use("/products", productRouter);
 // Health check route
 app.get("/health", (req, res) => {
   res.send("OK!");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err.message);
+  res.status(500).send({ message: err.message });
 });
 
 // Start the server
